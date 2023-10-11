9619c2651aa5e44b5735af51a45f484c68c1fd62
Dmitrii Alferev <dmitry@alferev.com>
2023-10-11 22:53:29 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/abdraAPI/api.js
+++ store/abdraAPI/api.js
@@ -1,77 +1,100 @@
-import axios from "axios"
+import axios from "axios";
 const abdraAPI = axios.create({
   baseURL: "https://abd-231006-ra1-2.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
-  return abdraAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+  return abdraAPI.get(`/api-docs/schema/`, {
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return abdraAPI.post(`/api/v1/login/`, payload)
+  return abdraAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return abdraAPI.post(`/api/v1/signup/`, payload)
+  return abdraAPI.post(`/api/v1/signup/`, payload);
 }
+
 function modules_two_factor_authentication_enable_2fa_retrieve(payload) {
-  return abdraAPI.get(`/modules/two-factor-authentication/enable/2fa`)
+  return abdraAPI.get(`/modules/two-factor-authentication/enable/2fa`);
 }
+
 function modules_two_factor_authentication_enable_2fa_create(payload) {
-  return abdraAPI.post(`/modules/two-factor-authentication/enable/2fa`)
+  return abdraAPI.post(`/modules/two-factor-authentication/enable/2fa`);
 }
+
 function modules_two_factor_authentication_enable_2fa_destroy(payload) {
-  return abdraAPI.delete(`/modules/two-factor-authentication/enable/2fa`)
+  return abdraAPI.delete(`/modules/two-factor-authentication/enable/2fa`);
 }
-function modules_two_factor_authentication_google_authenticator_qr_retrieve(
-  payload
-) {
-  return abdraAPI.get(
-    `/modules/two-factor-authentication/google/authenticator/qr`
-  )
+
+function modules_two_factor_authentication_google_authenticator_qr_retrieve(payload) {
+  return abdraAPI.get(`/modules/two-factor-authentication/google/authenticator/qr`);
 }
+
 function modules_two_factor_authentication_send_otp_create(payload) {
-  return abdraAPI.post(`/modules/two-factor-authentication/send/otp`)
+  return abdraAPI.post(`/modules/two-factor-authentication/send/otp`);
 }
+
 function modules_two_factor_authentication_verify_otp_create(payload) {
-  return abdraAPI.post(`/modules/two-factor-authentication/verify/otp`)
+  return abdraAPI.post(`/modules/two-factor-authentication/verify/otp`);
 }
+
 function modules_two_factor_authentication_verify_otp_create_2(payload) {
-  return abdraAPI.post(
-    `/modules/two-factor-authentication/verify/otp/${payload.enable}`
-  )
+  return abdraAPI.post(`/modules/two-factor-authentication/verify/otp/${payload.enable}`);
 }
+
 function rest_auth_login_create(payload) {
-  return abdraAPI.post(`/rest-auth/login/`, payload)
+  return abdraAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_create(payload) {
-  return abdraAPI.post(`/rest-auth/logout/`)
+  return abdraAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return abdraAPI.post(`/rest-auth/password/change/`, payload)
+  return abdraAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return abdraAPI.post(`/rest-auth/password/reset/`, payload)
+  return abdraAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return abdraAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return abdraAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return abdraAPI.post(`/rest-auth/registration/`, payload)
+  return abdraAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_resend_email_create(payload) {
-  return abdraAPI.post(`/rest-auth/registration/resend-email/`, payload)
+  return abdraAPI.post(`/rest-auth/registration/resend-email/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return abdraAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return abdraAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return abdraAPI.get(`/rest-auth/user/`)
+  return abdraAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return abdraAPI.put(`/rest-auth/user/`, payload)
+  return abdraAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return abdraAPI.patch(`/rest-auth/user/`, payload)
+  return abdraAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -94,4 +117,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
