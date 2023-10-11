3e19f3e62c3cb374972aee35276425d02df7f301
Dmitrii Alferev <dmitry@alferev.com>
2023-10-11 17:31:07 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ modules/contact-us/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigator from "./Navigator";
+import { slice } from "./store";
+
+const Home = () => {
+  return <Navigator />;
+};
+
+export default {
+  title: "Contact-Us",
+  navigator: Home,
+  slice
+};
